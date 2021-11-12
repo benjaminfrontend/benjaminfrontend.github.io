b81898b6f3fbe516921f322f91681eca949137f7
benjaminfrontend <94125892+benjaminfrontend@users.noreply.github.com>
2021-11-12 17:50:34 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,42 @@
+const txtAnim = document.querySelector('h4');
+
+new Typewriter(txtAnim, {
+    deleteSpeed: 20
+})
+
+.typeString('Hey ! Moi c\'et')
+.pauseFor(300)
+.deleteChars(9)
+.pause(1000)
+.typeString(' Moi c\'est')
+.start()
+
+const sr = ScrollReveal();
+
+sr.reveal('img', {
+    duration: 500,
+    scale: 0.5,
+    reset: true
+});
+
+sr.reveal('nav.nav', {
+    duration: 2000,
+    scale: 0.5,
+    reset: true
+});
+
+sr.reveal('.left', {
+    origin: 'top',
+    duration: 3000,
+    scale: 0.5,
+    reset: true
+});
+
+sr.reveal('.container', {
+    origin: 'top',
+    duration: 3000,
+    scale: 0.5,
+    reset: true
+});
+
+
